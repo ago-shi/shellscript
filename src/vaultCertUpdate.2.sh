0ccde5cdef8c102153d8c00c6788e256947260fb
ago-shi <technicolor.go+ict@gmail.com>
2025-01-26 15:56:11 +0000

    update: vaultCertUpdate.2.sh

--- src/vaultCertUpdate.2.sh
+++ src/vaultCertUpdate.2.sh
@@ -9,17 +9,69 @@
 
 CRT_PATH="/etc/pki/tls/certs/"
 CRT_FILE="vault01.uws.lan.crt"
-CRT_EXISTS="FALSE"
+CRT_EXIST="FALSE"
 PRIV_PATH="/etc/pki/tls/private/"
 PRIV_FILE="vault01.uws.lan.key"
-PRIV_EXISTS="FALSE"
+PRIV_EXIST="FALSE"
+CERT=""
+
+RENEW_BEFORE_EXPIRY=10
+EXPIRY=""
+UNX_CUR_DATE=""
+UPDATE_LIMIT=""
+MSG=""
+CRT_UPD="FALSE"
+
+# テストコード用変数
 
 # certファイルの存在確認
-if [ -e ${CRT_PATH}${CRT_FILE} ];then
-    CRT_EXISTS="TRUE"
+if CERT=$(openssl x509 -noout -dates -in ${CRT_PATH}${CRT_FILE} 2> /dev/null); 
+then
+    CRT_EXIST="TRUE"
+else
+    touch ${CRT_PATH}${CRT_FILE}
 fi
 
 # privateファイルの存在確認
-if [ -e ${PRIV_PATH}${PRIV_FILE} ]; then
-    PRIV_EXISTS="TRUE"
+if ls ${PRIV_PATH}${PRIV_FILE} > /dev/null 2>&1; 
+then
+    if openssl rsa -text < ${PRIV_PATH}${PRIV_FILE} > /dev/null 2>&1;
+    then
+        PRIV_EXIST="TRUE"
+    fi
+else
+    touch ${PRIV_PATH}${PRIV_FILE}
+fi
+
+# テストコード
+if [ "$1" = "test" ]; then
+    echo "CRT_EXIST is $CRT_EXIST"
+    echo "PRIV_EXIST is $PRIV_EXIST"
+    ls ${CRT_PATH}${CRT_FILE}
+    ls ${PRIV_PATH}${PRIV_FILE}
+fi
+
+if [ "${CRT_EXIST}" = "TRUE" ] && [ "${PRIV_EXIST}" = "TRUE" ]; then
+
+    EXPIRY=$(echo "$CERT" | grep "notAfter=" | awk -F'=' '{print $2}')
+    UNX_EXPIRY=$(date -d "${EXPIRY}" +%s)
+    UNX_CUR_DATE=$(date -d "$(date)" +%s)
+    UPDATE_LIMIT=$(( UNX_EXPIRY - RENEW_BEFORE_EXPIRY * 24 * 60 * 60 ))
+
+    if [ $((UPDATE_LIMIT)) -le $((UNX_CUR_DATE)) ]; then
+        MSG="[vault certification]certificate needs update."
+        CRT_UPD="TRUE"
+    else
+        MSG="[vault certification]certificate is not update yet."
+    fi
+else
+    MSG="dummy"
+fi
+
+logger "${MSG}"
+
+# テストコード
+if [ "$1" = "test" ]; then
+    echo "$MSG"
+    echo "CRT_UPD is ${CRT_UPD}."
 fi
--- test/vaultCertUpdate_test.sh
+++ test/vaultCertUpdate_test.sh
@@ -4,27 +4,130 @@ setup() {
     load 'test_helper/bats-support/load'
     load 'test_helper/bats-assert/load'
 
+    TGT_SHELL="vaultCertUpdate.2.sh test"
+    CERTP="/etc/pki/tls/certs/"
+    CERTF="vault01.uws.lan.crt"
+    PRIVP="/etc/pki/tls/private/"
+    PRIVF="vault01.uws.lan.key"
+    TMP="/tmp/"
+
     DIR="/bats/shell/test/"
     PATH="$DIR/../src/:$PATH"
 }
 
 teardown() {
-    rm -f /etc/pki/tls/certs/vault01.uws.lan.crt
-    rm -f /etc/pki/tls/private/vault01.uws.lan.key
+    rm -f "${CERTP}${CERTF}"
+    rm -f "${PRIVP}${PRIVF}"
 }
+
 @test "case.0 シェルスクリプトが実行可能" {
-    vaultCertUpdate.2.sh
+    $TGT_SHELL
 }
 
-@test "case.1-1 証明書ある & 鍵ある" {
-    touch /etc/pki/tls/certs/vault01.uws.lan.crt
-    touch /etc/pki/tls/private/vault01.uws.lan.key
+@test "case.1-1-1 証明書ある & 鍵ある" {
+    cp -f "${TMP}${CERTF}" "${CERTP}${CERTF}"
+    cp -f "${TMP}${PRIVF}" "${PRIVP}${PRIVF}"
 
-    run vaultCertUpdate.2.sh
-    [ "$status" -eq 0 ]
-    [[ "$CRT_EXISTS" == "TRUE" ]]
-    [[ "$PRIV_EXISTS" == "TRUE" ]]
+    run $TGT_SHELL
+    [ "${status}" -eq 0 ]
+    [ "${lines[0]}" = "CRT_EXIST is TRUE" ]
+    [ "${lines[1]}" = "PRIV_EXIST is TRUE" ]
+    [ "${lines[2]}" = "${CERTP}${CERTF}" ]
+    [ "${lines[3]}" = "${PRIVP}${PRIVF}" ]
+}
 
-    rm -f /etc/pki/tls/certs/vault01.uws.lan.crt
-    rm -f /etc/pki/tls/private/vault01.uws.lan.key
+@test "case.1-1-2 証明書ある & 鍵ない & 鍵と同名ファイルある" {
+    cp -f "${TMP}${CERTF}" "${CERTP}${CERTF}"
+    touch "${PRIVP}${PRIVF}"
+
+    run $TGT_SHELL
+    [ "${status}" -eq 0 ]
+    [ "${lines[0]}" = "CRT_EXIST is TRUE" ]
+    [ "${lines[1]}" = "PRIV_EXIST is FALSE" ]
+    [ "${lines[2]}" = "${CERTP}${CERTF}" ]
+    [ "${lines[3]}" = "${PRIVP}${PRIVF}" ]
+}
+
+@test "case.1-1-3 証明書ある & 鍵ない & 鍵と同名ファイルない" {
+    cp -f "${TMP}${CERTF}" "${CERTP}${CERTF}"
+    rm -f "${PRIVP}${PRIVF}"
+
+    run $TGT_SHELL
+    [ "${status}" -eq 0 ]
+    [ "${lines[0]}" = "CRT_EXIST is TRUE" ]
+    [ "${lines[1]}" = "PRIV_EXIST is FALSE" ]
+    [ "${lines[2]}" = "${CERTP}${CERTF}" ]
+    [ "${lines[3]}" = "${PRIVP}${PRIVF}" ]
+}
+
+@test "case.1-2-1 証明書ない & 証明書と同名ファイルある & 鍵ある" {
+    touch "${CERTP}${CERTF}"
+    cp -f "${TMP}${PRIVF}" "${PRIVP}${PRIVF}"
+
+    run $TGT_SHELL
+    [ "${status}" -eq 0 ]
+    [ "${lines[0]}" = "CRT_EXIST is FALSE" ]
+    [ "${lines[1]}" = "PRIV_EXIST is TRUE" ]
+    [ "${lines[2]}" = "${CERTP}${CERTF}" ]
+    [ "${lines[3]}" = "${PRIVP}${PRIVF}" ]
+}
+
+@test "case.1-2-2 証明書ない & 証明書と同名ファイルある & 鍵ない & 鍵と同盟ファイルある" {
+    touch "${CERTP}${CERTF}"
+    touch "${PRIVP}${PRIVF}"
+
+    run $TGT_SHELL
+    [ "${status}" -eq 0 ]
+    [ "${lines[0]}" = "CRT_EXIST is FALSE" ]
+    [ "${lines[1]}" = "PRIV_EXIST is FALSE" ]
+    [ "${lines[2]}" = "${CERTP}${CERTF}" ]
+    [ "${lines[3]}" = "${PRIVP}${PRIVF}" ]
+}
+
+@test "case.1-2-3 証明書ない & 証明書と同名ファイルある & 鍵ない & 鍵と同盟ファイルない" {
+    touch "${CERTP}${CERTF}"
+    rm -f "${PRIVP}${PRIVF}"
+
+    run $TGT_SHELL
+    [ "${status}" -eq 0 ]
+    [ "${lines[0]}" = "CRT_EXIST is FALSE" ]
+    [ "${lines[1]}" = "PRIV_EXIST is FALSE" ]
+    [ "${lines[2]}" = "${CERTP}${CERTF}" ]
+    [ "${lines[3]}" = "${PRIVP}${PRIVF}" ]
+}
+
+@test "case.1-3-1 証明書ない & 証明書と同名ファイルない & 鍵ある" {
+    rm -f "${CERTP}${CERTF}"
+    cp -f "${TMP}${PRIVF}" "${PRIVP}${PRIVF}"
+
+    run $TGT_SHELL
+    [ "${status}" -eq 0 ]
+    [ "${lines[0]}" = "CRT_EXIST is FALSE" ]
+    [ "${lines[1]}" = "PRIV_EXIST is TRUE" ]
+    [ "${lines[2]}" = "${CERTP}${CERTF}" ]
+    [ "${lines[3]}" = "${PRIVP}${PRIVF}" ]
+}
+
+@test "case.1-3-2 証明書ない & 証明書と同名ファイルない & 鍵ない & 鍵と同盟ファイルある" {
+    rm -f "${CERTP}${CERTF}"
+    touch "${PRIVP}${PRIVF}"
+
+    run $TGT_SHELL
+    [ "${status}" -eq 0 ]
+    [ "${lines[0]}" = "CRT_EXIST is FALSE" ]
+    [ "${lines[1]}" = "PRIV_EXIST is FALSE" ]
+    [ "${lines[2]}" = "${CERTP}${CERTF}" ]
+    [ "${lines[3]}" = "${PRIVP}${PRIVF}" ]
+}
+
+@test "case.1-3-3 証明書ない & 証明書と同名ファイルない & 鍵ない & 鍵と同盟ファイルない" {
+    rm -f "${CERTP}${CERTF}"
+    rm -f "${PRIVP}${PRIVF}"
+
+    run $TGT_SHELL
+    [ "${status}" -eq 0 ]
+    [ "${lines[0]}" = "CRT_EXIST is FALSE" ]
+    [ "${lines[1]}" = "PRIV_EXIST is FALSE" ]
+    [ "${lines[2]}" = "${CERTP}${CERTF}" ]
+    [ "${lines[3]}" = "${PRIVP}${PRIVF}" ]
 }
