ba3276775941876b8e243e8fe0bfe06de262d5b1
ago-shi <technicolor.go+ict@gmail.com>
2025-02-03 16:00:45 +0000

    update: finish test

--- src/vaultCertUpdate.2.sh
+++ src/vaultCertUpdate.2.sh
@@ -7,71 +7,209 @@
 ## unseal keyを特定のディレクトリにファイルで保存しておくこと。　           ##
 #########################################################################
 
+# サーバのパラメータ
+HOSTNAME="vault01"
+IPADDR="192.168.1.69"
+
+# hashicorp vaultのパラメータ
+PKI_PATH="pki_int/issue/uws-dot-lan"
+CMN_NAME="${HOSTNAME}.uws.lan"
+
+# 証明書、秘密鍵の変数
 CRT_PATH="/etc/pki/tls/certs/"
-CRT_FILE="vault01.uws.lan.crt"
+CRT_FILE="${CRT_PATH}${CMN_NAME}.crt"
+CRT_OLD="${CRT_FILE}.old"
 CRT_EXIST="FALSE"
 PRIV_PATH="/etc/pki/tls/private/"
-PRIV_FILE="vault01.uws.lan.key"
+PRIV_FILE="${PRIV_PATH}${CMN_NAME}.key"
+PRIV_OLD="${PRIV_FILE}.old"
 PRIV_EXIST="FALSE"
 CERT=""
+ALT_NAMES="DNS:${HOSTNAME},IP:${IPADDR}"
 
 RENEW_BEFORE_EXPIRY=10
 EXPIRY=""
-UNX_CUR_DATE=""
+EPOC_DATE=""
 UPDATE_LIMIT=""
 MSG=""
 CRT_UPD="FALSE"
-
-# テストコード用変数
+CRT_JSN=""
+EXIT="0"
+COUNT=0
+UNSEAL="/root/.vaultUnsealKey"
 
 # certファイルの存在確認
-if CERT=$(openssl x509 -noout -dates -in ${CRT_PATH}${CRT_FILE} 2> /dev/null); 
+if CERT=$(openssl x509 -noout -dates -in ${CRT_FILE} 2> /dev/null); 
 then
     CRT_EXIST="TRUE"
 else
-    touch ${CRT_PATH}${CRT_FILE}
+    touch ${CRT_FILE}
 fi
 
 # privateファイルの存在確認
-if ls ${PRIV_PATH}${PRIV_FILE} > /dev/null 2>&1; 
+if ls ${PRIV_FILE} > /dev/null 2>&1; 
 then
-    if openssl rsa -text < ${PRIV_PATH}${PRIV_FILE} > /dev/null 2>&1;
+    if openssl rsa -text < ${PRIV_FILE} > /dev/null 2>&1;
     then
         PRIV_EXIST="TRUE"
     fi
 else
-    touch ${PRIV_PATH}${PRIV_FILE}
+    touch ${PRIV_FILE}
 fi
 
 # テストコード
 if [ "$1" = "test" ]; then
     echo "CRT_EXIST is $CRT_EXIST"
     echo "PRIV_EXIST is $PRIV_EXIST"
-    ls ${CRT_PATH}${CRT_FILE}
-    ls ${PRIV_PATH}${PRIV_FILE}
+    ls ${CRT_FILE}
+    ls ${PRIV_FILE}
 fi
 
 if [ "${CRT_EXIST}" = "TRUE" ] && [ "${PRIV_EXIST}" = "TRUE" ]; then
-
     EXPIRY=$(echo "$CERT" | grep "notAfter=" | awk -F'=' '{print $2}')
-    UNX_EXPIRY=$(date -d "${EXPIRY}" +%s)
-    UNX_CUR_DATE=$(date -d "$(date)" +%s)
-    UPDATE_LIMIT=$(( UNX_EXPIRY - RENEW_BEFORE_EXPIRY * 24 * 60 * 60 ))
+    EPOC_EXPIRY=$(date -d "${EXPIRY}" +%s)
+    EPOC_DATE=$(date -d "$(date)" +%s)
+    UPDATE_LIMIT=$((EPOC_EXPIRY - RENEW_BEFORE_EXPIRY * 24 * 60 * 60))
 
-    if [ $((UPDATE_LIMIT)) -le $((UNX_CUR_DATE)) ]; then
+    # テストコード
+    if [ "$1" = "test" ]; then
+        EPOC_DATE="$(cat ./test/mock/dumDate.txt)"
+        UPDATE_LIMIT="$(cat ./test/mock/dumLimit.txt)"
+    fi
+
+    if [ $((UPDATE_LIMIT)) -le $((EPOC_DATE)) ]; then
         MSG="[vault certification]certificate needs update."
         CRT_UPD="TRUE"
     else
         MSG="[vault certification]certificate is not update yet."
+        CRT_UPD="FALSE"
     fi
 else
-    MSG="dummy"
+    MSG="[vault certification]certificate needs creating."
+    CRT_UPD="TRUE"
 fi
-
-logger "${MSG}"
 
 # テストコード
 if [ "$1" = "test" ]; then
     echo "$MSG"
-    echo "CRT_UPD is ${CRT_UPD}."
+    echo "CRT_UPD is $CRT_UPD."
 fi
+
+logger "$MSG"
+
+if [ $CRT_UPD = "TRUE" ]; then
+    # 本番環境用コード
+    if [ "$1" != "test" ]; then
+        CRT_JSN=$(vault write -format=json "$PKI_PATH" \
+        common_name="$CMN_NAME" alt_names="$ALT_NAMES")
+        RTN="$?"
+    # テストコード
+    else
+        CRT_JSN=$(cat "./test/mock/dum${HOSTNAME}.json")
+        RTN=$(cat "./test/mock/dumRTN.txt")
+    fi
+
+    if [ "$RTN" = "0" ]; then
+        rm -f "$CRT_OLD" "$PRIV_OLD"
+        mv "$CRT_FILE" "$CRT_OLD"
+        mv "$PRIV_FILE" "$PRIV_OLD"
+
+        echo "$CRT_JSN" | jq -r '.data.private_key' > $PRIV_FILE
+        echo "$CRT_JSN" | jq -r '.data.certificate' > $CRT_FILE
+        echo "$CRT_JSN" | jq -r '.data.issuing_ca' >> $CRT_FILE
+
+        MSG="[vault certification]cerfification update is succeeded."
+        EXIT="notExit"
+    else
+        MSG="[vault certification]cerfification update is abnormal end."
+        EXIT="5"
+    fi
+else
+    MSG="[vault certification]cerfification update process is end."
+    EXIT="0"
+fi
+
+logger "$MSG"
+
+# テストコード
+if [ "$1" = "test" ]; then
+    echo "$EXIT"
+    echo "$MSG"
+fi
+
+if [ "$EXIT" != "notExit" ]; then
+    exit $((EXIT))
+fi
+
+# 本番用コード
+if [ "$1" != "test" ]; then
+    systemctl restart vault >/dev/null 2>&1
+    RTN="$?"
+# テストコード
+else
+    RTN=$(cat "./test/mock/dumVaultRTN.txt")
+fi
+
+if [ "$RTN" = "0" ]; then 
+    MSG="[vault certification]vault is restarted."
+    EXIT="notExit"
+else
+    MSG="[vault certification]vault restart is abnormal end."
+    EXIT="100"
+fi
+
+logger "$MSG"
+
+# テストコード line[8] & line[9]
+if [ "$1" = "test" ]; then
+    echo "$EXIT"
+    echo "$MSG"
+fi
+
+if [ "$EXIT" != "notExit" ]; then
+    exit $((EXIT))
+fi
+
+COUNT=0
+while IFS= read -r line
+do
+    if [ $((COUNT)) -lt 3 ]; then
+        # 本番用コード
+        if [ "$1" != "test" ]; then
+            vault operator unseal "$line"
+            RTN="$?"
+        # テストコード
+        else
+            RTN=$(cat "./test/mock/dumUnsealRTN.txt")
+        fi
+
+        if [ "$RTN" = "0" ]; then
+            ((++COUNT))
+        else
+            logger "[vault certification]warning vault unseal is failed.(line=$COUNT)"
+        fi
+    fi
+done < $UNSEAL 
+
+# テストコード line[10]
+if [ "$1" = "test" ]; then
+    echo "COUNT is ${COUNT}."
+    COUNT=$(cat "./test/mock/dumCountRTN.txt")
+fi
+
+if [ $((COUNT)) -eq 3 ]; then
+    MSG="[vault certification]vault unseal is normal end."
+    EXIT="0"
+else
+    MSG="[vault certification]vault unseal is abnormal end."
+    EXIT="101"
+fi
+
+logger "$MSG"
+
+# テストコード line[11]
+if [ "$1" = "test" ]; then
+    echo "$MSG"
+fi
+
+exit $((EXIT))
--- test/vaultCertUpdate_test_bk.sh
+++ test/vaultCertUpdate_test_bk.sh
@@ -37,26 +37,20 @@ teardown() {
     rm -f "${MOCK_DIR}"dum*.json
 }
 
-@test "case.1-1 正常系 - 有効期限到来" {
-    ## input 
-    # 証明書と鍵の両方が存在している
-    # 証明書の有効期限が到来している(UPDATE_LIMIT < EPOC_DATE)
+@test "case.0 シェルスクリプトが実行可能" {
+    skip
+    $TGT_SHELL
+}
+
+@test "case.1-1-1 証明書ある & 鍵ある & 証明書更新成功" {
+    skip "case.2-1-1 ~ 2-1-2にマージ"
     cp -f "${TMP}${CERTF}" "${CERTP}${CERTF}"
     cp -f "${TMP}${PRIVF}" "${PRIVP}${PRIVF}"
 
-    ## dummyデータ
-    # 証明書の有効期限到来(UPDATE_LIMIT < EPOC_DATE)
-    echo "2864000" >> "${MOCK_DIR}dumDate.txt"
-    echo "2564000" >> "${MOCK_DIR}dumLimit.txt"
-    # 証明書作成成功
+    echo "1864000" > "${MOCK_DIR}dumDate.txt"
+    echo "1864000" > "${MOCK_DIR}dumLimit.txt"
     echo "0" > "${MOCK_DIR}dumRTN.txt"
-    # vault restart成功
-    echo "0" > "${MOCK_DIR}dumVaultRTN.txt"
-    # vault unseal成功
-    echo "0" > "${MOCK_DIR}dumUnsealRTN.txt"
-    echo "3" > "${MOCK_DIR}dumCountRTN.txt"
 
-    ## output
     run $TGT_SHELL
     [ "${status}" -eq 0 ]
     [ "${lines[0]}" = "CRT_EXIST is TRUE" ]
@@ -65,155 +59,29 @@ teardown() {
     [ "${lines[3]}" = "${PRIVP}${PRIVF}" ]
     [ "${lines[4]}" = "[vault certification]certificate needs update." ]
     [ "${lines[5]}" = "CRT_UPD is TRUE." ]
-    [ "${lines[6]}" = "notExit" ]
-    [ "${lines[7]}" = "[vault certification]cerfification update is succeeded." ]
-    [ "${lines[8]}" = "notExit" ]
-    [ "${lines[9]}" = "[vault certification]vault is restarted." ]
-    [ "${lines[10]}" = "COUNT is 3." ]
-    [ "${lines[11]}" = "[vault certification]vault unseal is normal end." ]
-}
-
-@test "case.1-2 正常系 - 有効期限未到来" {
-    ## input 
-    # 証明書と鍵の両方が存在している
-    # 証明書の有効期限が到来していない(UPDATE_LIMIT > EPOC_DATE)
-    cp -f "${TMP}${CERTF}" "${CERTP}${CERTF}"
-    cp -f "${TMP}${PRIVF}" "${PRIVP}${PRIVF}"
-
-    ## dummyデータ
-    # 証明書の有効期限到来(UPDATE_LIMIT > EPOC_DATE)
-    echo "1864000" >> "${MOCK_DIR}dumDate.txt"
-    echo "2864000" >> "${MOCK_DIR}dumLimit.txt"
-
-    ## output
-    run $TGT_SHELL
-    [ "${status}" -eq 0 ]
-    [ "${lines[0]}" = "CRT_EXIST is TRUE" ]
-    [ "${lines[1]}" = "PRIV_EXIST is TRUE" ]
-    [ "${lines[2]}" = "${CERTP}${CERTF}" ]
-    [ "${lines[3]}" = "${PRIVP}${PRIVF}" ]
-    [ "${lines[4]}" = "[vault certification]certificate is not update yet." ]
-    [ "${lines[5]}" = "CRT_UPD is FALSE." ]
     [ "${lines[6]}" = "0" ]
-    [ "${lines[7]}" = "[vault certification]cerfification update process is end." ]
+    [ "${lines[7]}" = "[vault certification]cerfification update is succeeded." ]
 }
 
-@test "case.1-3-1 正常系 - 不正な証明書あり・不正な鍵あり" {
-    touch "${CERTP}${CERTF}"
-    touch "${PRIVP}${PRIVF}"
-
-    echo "0" > "${MOCK_DIR}dumRTN.txt"
-    echo "0" > "${MOCK_DIR}dumVaultRTN.txt"
-    echo "0" > "${MOCK_DIR}dumUnsealRTN.txt"
-    echo "3" > "${MOCK_DIR}dumCountRTN.txt"
-
-    run $TGT_SHELL
-    [ "${status}" -eq 0 ]
-    [ "${lines[0]}" = "CRT_EXIST is FALSE" ]
-    [ "${lines[1]}" = "PRIV_EXIST is FALSE" ]
-    [ "${lines[2]}" = "${CERTP}${CERTF}" ]
-    [ "${lines[3]}" = "${PRIVP}${PRIVF}" ]
-    [ "${lines[4]}" = "[vault certification]certificate needs creating." ]
-    [ "${lines[5]}" = "CRT_UPD is TRUE." ]
-}
-
-@test "case.1-3-2 正常系 - 不正な証明書あり・鍵なし" {
-    touch "${CERTP}${CERTF}"
-    rm -f "${PRIVP}${PRIVF}"
-
-    echo "0" > "${MOCK_DIR}dumRTN.txt"
-    echo "0" > "${MOCK_DIR}dumVaultRTN.txt"
-    echo "0" > "${MOCK_DIR}dumUnsealRTN.txt"
-    echo "3" > "${MOCK_DIR}dumCountRTN.txt"
-
-    run $TGT_SHELL
-    [ "${status}" -eq 0 ]
-    [ "${lines[0]}" = "CRT_EXIST is FALSE" ]
-    [ "${lines[1]}" = "PRIV_EXIST is FALSE" ]
-    [ "${lines[2]}" = "${CERTP}${CERTF}" ]
-    [ "${lines[3]}" = "${PRIVP}${PRIVF}" ]
-    [ "${lines[4]}" = "[vault certification]certificate needs creating." ]
-    [ "${lines[5]}" = "CRT_UPD is TRUE." ]
-}
-
-@test "case.1-3-3 正常系 - 証明書なし・鍵あり" {
-    rm -f "${CERTP}${CERTF}"
-    cp -f "${TMP}${PRIVF}" "${PRIVP}${PRIVF}"
-
-    echo "0" > "${MOCK_DIR}dumRTN.txt"
-    echo "0" > "${MOCK_DIR}dumVaultRTN.txt"
-    echo "0" > "${MOCK_DIR}dumUnsealRTN.txt"
-    echo "3" > "${MOCK_DIR}dumCountRTN.txt"
-
-    run $TGT_SHELL
-    [ "${status}" -eq 0 ]
-    [ "${lines[0]}" = "CRT_EXIST is FALSE" ]
-    [ "${lines[1]}" = "PRIV_EXIST is TRUE" ]
-    [ "${lines[2]}" = "${CERTP}${CERTF}" ]
-    [ "${lines[3]}" = "${PRIVP}${PRIVF}" ]
-    [ "${lines[4]}" = "[vault certification]certificate needs creating." ]
-    [ "${lines[5]}" = "CRT_UPD is TRUE." ]
-}
-
-@test "case.1-3-4 正常系 - 証明書なし・不正な鍵あり" {
-    rm -f "${CERTP}${CERTF}"
-    touch "${PRIVP}${PRIVF}"
-
-    echo "0" > "${MOCK_DIR}dumRTN.txt"
-    echo "0" > "${MOCK_DIR}dumVaultRTN.txt"
-    echo "0" > "${MOCK_DIR}dumUnsealRTN.txt"
-    echo "3" > "${MOCK_DIR}dumCountRTN.txt"
-
-    run $TGT_SHELL
-    [ "${status}" -eq 0 ]
-    [ "${lines[0]}" = "CRT_EXIST is FALSE" ]
-    [ "${lines[1]}" = "PRIV_EXIST is FALSE" ]
-    [ "${lines[2]}" = "${CERTP}${CERTF}" ]
-    [ "${lines[3]}" = "${PRIVP}${PRIVF}" ]
-    [ "${lines[4]}" = "[vault certification]certificate needs creating." ]
-    [ "${lines[5]}" = "CRT_UPD is TRUE." ]
-}
-
-@test "case.1-3-5 正常系 - 証明書なし・鍵なし" {
-    rm -f "${CERTP}${CERTF}"
-    rm -f "${PRIVP}${PRIVF}"
-
-    echo "0" > "${MOCK_DIR}dumRTN.txt"
-    echo "0" > "${MOCK_DIR}dumVaultRTN.txt"
-    echo "0" > "${MOCK_DIR}dumUnsealRTN.txt"
-    echo "3" > "${MOCK_DIR}dumCountRTN.txt"
-
-    run $TGT_SHELL
-    [ "${status}" -eq 0 ]
-    [ "${lines[0]}" = "CRT_EXIST is FALSE" ]
-    [ "${lines[1]}" = "PRIV_EXIST is FALSE" ]
-    [ "${lines[2]}" = "${CERTP}${CERTF}" ]
-    [ "${lines[3]}" = "${PRIVP}${PRIVF}" ]
-    [ "${lines[4]}" = "[vault certification]certificate needs creating." ]
-    [ "${lines[5]}" = "CRT_UPD is TRUE." ]
-}
-
-@test "case.2-1 異常系 - 証明書作成失敗 + 証明書有効期限チェック同値テスト" {
+@test "case.1-1-2 証明書ある & 鍵ない & 鍵と同名ファイルある & 証明書更新失敗" {
     cp -f "${TMP}${CERTF}" "${CERTP}${CERTF}"
-    cp -f "${TMP}${PRIVF}" "${PRIVP}${PRIVF}"
+    touch "${PRIVP}${PRIVF}"
 
-    echo "1864000" >> "${MOCK_DIR}dumDate.txt"
-    echo "1864000" >> "${MOCK_DIR}dumLimit.txt"
     echo "1" > "${MOCK_DIR}dumRTN.txt"
 
     run $TGT_SHELL
     [ "${status}" -eq 5 ]
     [ "${lines[0]}" = "CRT_EXIST is TRUE" ]
-    [ "${lines[1]}" = "PRIV_EXIST is TRUE" ]
+    [ "${lines[1]}" = "PRIV_EXIST is FALSE" ]
     [ "${lines[2]}" = "${CERTP}${CERTF}" ]
     [ "${lines[3]}" = "${PRIVP}${PRIVF}" ]
-    [ "${lines[4]}" = "[vault certification]certificate needs update." ]
+    [ "${lines[4]}" = "[vault certification]certificate needs creating." ]
     [ "${lines[5]}" = "CRT_UPD is TRUE." ]
     [ "${lines[6]}" = "5" ]
     [ "${lines[7]}" = "[vault certification]cerfification update is abnormal end." ]
 }
 
-@test "case.2-2 異常系 - vault再起動失敗" {
+@test "case.1-1-3 証明書ある & 鍵ない & 鍵と同名ファイルない & vaultリスタート失敗" {
     cp -f "${TMP}${CERTF}" "${CERTP}${CERTF}"
     rm -f "${PRIVP}${PRIVF}"
 
@@ -234,7 +102,7 @@ teardown() {
     [ "${lines[9]}" = "[vault certification]vault restart is abnormal end." ]
 }
 
-@test "case.2-3 異常系 - vault unseal失敗" {
+@test "case.1-2-1 証明書ない & 証明書と同名ファイルある & 鍵ある" {
     touch "${CERTP}${CERTF}"
     cp -f "${TMP}${PRIVF}" "${PRIVP}${PRIVF}"
 
@@ -258,4 +126,169 @@ teardown() {
     [ "${lines[10]}" = "COUNT is 0." ]
     [ "${lines[11]}" = "[vault certification]vault unseal is abnormal end." ]
 
+}
+
+@test "case.1-2-2 証明書ない & 証明書と同名ファイルある & 鍵ない & 鍵と同名ファイルある" {
+    touch "${CERTP}${CERTF}"
+    touch "${PRIVP}${PRIVF}"
+
+    echo "0" > "${MOCK_DIR}dumRTN.txt"
+    echo "0" > "${MOCK_DIR}dumVaultRTN.txt"
+    echo "0" > "${MOCK_DIR}dumUnsealRTN.txt"
+    echo "3" > "${MOCK_DIR}dumCountRTN.txt"
+
+    run $TGT_SHELL
+    [ "${status}" -eq 0 ]
+    [ "${lines[0]}" = "CRT_EXIST is FALSE" ]
+    [ "${lines[1]}" = "PRIV_EXIST is FALSE" ]
+    [ "${lines[2]}" = "${CERTP}${CERTF}" ]
+    [ "${lines[3]}" = "${PRIVP}${PRIVF}" ]
+    [ "${lines[4]}" = "[vault certification]certificate needs creating." ]
+    [ "${lines[5]}" = "CRT_UPD is TRUE." ]
+}
+
+@test "case.1-2-3 証明書ない & 証明書と同名ファイルある & 鍵ない & 鍵と同名ファイルない" {
+    touch "${CERTP}${CERTF}"
+    rm -f "${PRIVP}${PRIVF}"
+
+    echo "0" > "${MOCK_DIR}dumRTN.txt"
+    echo "0" > "${MOCK_DIR}dumVaultRTN.txt"
+    echo "0" > "${MOCK_DIR}dumUnsealRTN.txt"
+    echo "3" > "${MOCK_DIR}dumCountRTN.txt"
+
+    run $TGT_SHELL
+    [ "${status}" -eq 0 ]
+    [ "${lines[0]}" = "CRT_EXIST is FALSE" ]
+    [ "${lines[1]}" = "PRIV_EXIST is FALSE" ]
+    [ "${lines[2]}" = "${CERTP}${CERTF}" ]
+    [ "${lines[3]}" = "${PRIVP}${PRIVF}" ]
+    [ "${lines[4]}" = "[vault certification]certificate needs creating." ]
+    [ "${lines[5]}" = "CRT_UPD is TRUE." ]
+}
+
+@test "case.1-3-1 証明書ない & 証明書と同名ファイルない & 鍵ある" {
+    rm -f "${CERTP}${CERTF}"
+    cp -f "${TMP}${PRIVF}" "${PRIVP}${PRIVF}"
+
+    echo "0" > "${MOCK_DIR}dumRTN.txt"
+    echo "0" > "${MOCK_DIR}dumVaultRTN.txt"
+    echo "0" > "${MOCK_DIR}dumUnsealRTN.txt"
+    echo "3" > "${MOCK_DIR}dumCountRTN.txt"
+
+    run $TGT_SHELL
+    [ "${status}" -eq 0 ]
+    [ "${lines[0]}" = "CRT_EXIST is FALSE" ]
+    [ "${lines[1]}" = "PRIV_EXIST is TRUE" ]
+    [ "${lines[2]}" = "${CERTP}${CERTF}" ]
+    [ "${lines[3]}" = "${PRIVP}${PRIVF}" ]
+    [ "${lines[4]}" = "[vault certification]certificate needs creating." ]
+    [ "${lines[5]}" = "CRT_UPD is TRUE." ]
+}
+
+@test "case.1-3-2 証明書ない & 証明書と同名ファイルない & 鍵ない & 鍵と同名ファイルある" {
+    rm -f "${CERTP}${CERTF}"
+    touch "${PRIVP}${PRIVF}"
+
+    echo "0" > "${MOCK_DIR}dumRTN.txt"
+    echo "0" > "${MOCK_DIR}dumVaultRTN.txt"
+    echo "0" > "${MOCK_DIR}dumUnsealRTN.txt"
+    echo "3" > "${MOCK_DIR}dumCountRTN.txt"
+
+    run $TGT_SHELL
+    [ "${status}" -eq 0 ]
+    [ "${lines[0]}" = "CRT_EXIST is FALSE" ]
+    [ "${lines[1]}" = "PRIV_EXIST is FALSE" ]
+    [ "${lines[2]}" = "${CERTP}${CERTF}" ]
+    [ "${lines[3]}" = "${PRIVP}${PRIVF}" ]
+    [ "${lines[4]}" = "[vault certification]certificate needs creating." ]
+    [ "${lines[5]}" = "CRT_UPD is TRUE." ]
+}
+
+@test "case.1-3-3 証明書ない & 証明書と同名ファイルない & 鍵ない & 鍵と同名ファイルない" {
+    rm -f "${CERTP}${CERTF}"
+    rm -f "${PRIVP}${PRIVF}"
+
+    echo "0" > "${MOCK_DIR}dumRTN.txt"
+    echo "0" > "${MOCK_DIR}dumVaultRTN.txt"
+    echo "0" > "${MOCK_DIR}dumUnsealRTN.txt"
+    echo "3" > "${MOCK_DIR}dumCountRTN.txt"
+
+    run $TGT_SHELL
+    [ "${status}" -eq 0 ]
+    [ "${lines[0]}" = "CRT_EXIST is FALSE" ]
+    [ "${lines[1]}" = "PRIV_EXIST is FALSE" ]
+    [ "${lines[2]}" = "${CERTP}${CERTF}" ]
+    [ "${lines[3]}" = "${PRIVP}${PRIVF}" ]
+    [ "${lines[4]}" = "[vault certification]certificate needs creating." ]
+    [ "${lines[5]}" = "CRT_UPD is TRUE." ]
+}
+
+@test "case.2-1-1 証明書ある・鍵ある & (UPDATE_LIMIT < UNX_CUR_DATE) & 証明書更新成功 & vault restart/unseal成功" {
+    cp -f "${TMP}${CERTF}" "${CERTP}${CERTF}"
+    cp -f "${TMP}${PRIVF}" "${PRIVP}${PRIVF}"
+
+    echo "2864000" >> "${MOCK_DIR}dumDate.txt"
+    echo "2564000" >> "${MOCK_DIR}dumLimit.txt"
+    echo "0" > "${MOCK_DIR}dumRTN.txt"
+    echo "0" > "${MOCK_DIR}dumVaultRTN.txt"
+    echo "0" > "${MOCK_DIR}dumUnsealRTN.txt"
+    echo "3" > "${MOCK_DIR}dumCountRTN.txt"
+
+    run $TGT_SHELL
+    [ "${status}" -eq 0 ]
+    [ "${lines[0]}" = "CRT_EXIST is TRUE" ]
+    [ "${lines[1]}" = "PRIV_EXIST is TRUE" ]
+    [ "${lines[2]}" = "${CERTP}${CERTF}" ]
+    [ "${lines[3]}" = "${PRIVP}${PRIVF}" ]
+    [ "${lines[4]}" = "[vault certification]certificate needs update." ]
+    [ "${lines[5]}" = "CRT_UPD is TRUE." ]
+    [ "${lines[6]}" = "notExit" ]
+    [ "${lines[7]}" = "[vault certification]cerfification update is succeeded." ]
+    [ "${lines[8]}" = "notExit" ]
+    [ "${lines[9]}" = "[vault certification]vault is restarted." ]
+    [ "${lines[10]}" = "COUNT is 3." ]
+    [ "${lines[11]}" = "[vault certification]vault unseal is normal end." ]
+}
+
+@test "case.2-1-2 証明書ある・鍵ある & (UPDATE_LIMIT = UNX_CUR_DATE) & 証明書更新失敗" {
+    cp -f "${TMP}${CERTF}" "${CERTP}${CERTF}"
+    cp -f "${TMP}${PRIVF}" "${PRIVP}${PRIVF}"
+
+    echo "1864000" >> "${MOCK_DIR}dumDate.txt"
+    echo "1864000" >> "${MOCK_DIR}dumLimit.txt"
+    echo "1" > "${MOCK_DIR}dumRTN.txt"
+
+    run $TGT_SHELL
+    [ "${status}" -eq 5 ]
+    [ "${lines[0]}" = "CRT_EXIST is TRUE" ]
+    [ "${lines[1]}" = "PRIV_EXIST is TRUE" ]
+    [ "${lines[2]}" = "${CERTP}${CERTF}" ]
+    [ "${lines[3]}" = "${PRIVP}${PRIVF}" ]
+    [ "${lines[4]}" = "[vault certification]certificate needs update." ]
+    [ "${lines[5]}" = "CRT_UPD is TRUE." ]
+    [ "${lines[6]}" = "5" ]
+    [ "${lines[7]}" = "[vault certification]cerfification update is abnormal end." ]
+}
+
+@test "case.2-1-3 証明書ある・鍵ある & (UPDATE_LIMIT > UNX_CUR_DATE)" {
+    cp -f "${TMP}${CERTF}" "${CERTP}${CERTF}"
+    cp -f "${TMP}${PRIVF}" "${PRIVP}${PRIVF}"
+
+    echo "1864000" >> "${MOCK_DIR}dumDate.txt"
+    echo "2864000" >> "${MOCK_DIR}dumLimit.txt"
+
+    run $TGT_SHELL
+    [ "${status}" -eq 0 ]
+    [ "${lines[0]}" = "CRT_EXIST is TRUE" ]
+    [ "${lines[1]}" = "PRIV_EXIST is TRUE" ]
+    [ "${lines[2]}" = "${CERTP}${CERTF}" ]
+    [ "${lines[3]}" = "${PRIVP}${PRIVF}" ]
+    [ "${lines[4]}" = "[vault certification]certificate is not update yet." ]
+    [ "${lines[5]}" = "CRT_UPD is FALSE." ]
+    [ "${lines[6]}" = "0" ]
+    [ "${lines[7]}" = "[vault certification]cerfification update process is end." ]
+}
+
+@test "case.2-2-1 証明書ない・鍵ない" {
+    skip "case.1-3-3にマージ"
 }
